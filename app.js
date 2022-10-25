6c0ebf4b1ceb25b721e4159785b9b16f2f11781a
brittney-johnson <johnson_brittney1@columbusstate.edu>
2022-10-25 17:00:58 +0000

    fixed comic refresh

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const axios = require('axios');
 app.set("view engine", "ejs");
 app.use('/static', express.static("public"));
 
-function between(min, max) {  
-    return Math.floor(
-      Math.random() * (max - min) + min
-    )
-  }
-
-  imgURL = ["https://xkcd.com/{}/info.0.json".replace("{}", between(1,999))]
+// function between(min, max) {  
+//     return Math.floor(
+//       Math.random() * (max - min) + min
+//     )
+//   }
 
 
 app.get('/', function (req, res) {
+     random = Math.floor(Math.random() * 2500) +1;
+
+let imgURL = `https://xkcd.com/${random}/info.0.json`;
     axios.get(imgURL).then(function (response) {
         res.render('todo.ejs', { comicData: response.data });
     });
